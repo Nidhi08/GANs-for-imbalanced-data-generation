3223d8a64fdd3d54ef63200c8ee21d583111b3c5
Nidhi08 <nidhisakhala08@gmail.com>
2018-10-16 22:01:42 -0500

    Add files via upload
    utils.py - Code to log information for every epoch.
    GAN.py - GAN code
    report_mnist - explanation of code

---
+++ GAN.py
@@ -0,0 +1,291 @@
+
+# coding: utf-8
+
+# In[13]:
+
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.autograd import Variable
+from torchvision import transforms
+import torchvision
+import time
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from torch.utils.data.dataset import Dataset
+import torchvision.models as models
+import os
+from utils import Logger
+
+
+# In[2]:
+
+
+manualSeed = 999
+#manualSeed = random.randint(1, 10000) # use if you want new results
+print("Random Seed: ", manualSeed)
+random.seed(manualSeed)
+torch.manual_seed(manualSeed)
+
+
+# In[11]:
+
+
+def mnist_data():
+    compose = transforms.Compose(
+        [transforms.ToTensor(),
+         transforms.Normalize((.5, .5, .5), (.5, .5, .5))
+        ])
+    out_dir = '~/Documents/DL/hw4/nsakhala_HW04/mnist/'
+    return torchvision.datasets.MNIST(root=out_dir, train=True, transform=compose, download=True)
+
+
+# In[12]:
+
+
+data = mnist_data()
+
+
+# In[14]:
+
+
+# Create loader with data, so that we can iterate over it
+data_loader = torch.utils.data.DataLoader(data, batch_size=100, shuffle=True)
+# Num batches
+num_batches = len(data_loader)
+
+
+# In[15]:
+
+
+print (num_batches)
+
+
+# In[16]:
+
+
+class DiscriminatorNet(nn.Module):
+    """
+    A three hidden-layer discriminative neural network
+    """
+    def __init__(self):
+        super(DiscriminatorNet, self).__init__()
+        n_features = 784
+        n_out = 1
+        
+        self.hidden0 = nn.Sequential( 
+            nn.Linear(n_features, 1024),
+            nn.LeakyReLU(0.2),
+            nn.Dropout(0.3)
+        )
+        self.hidden1 = nn.Sequential(
+            nn.Linear(1024, 512),
+            nn.LeakyReLU(0.2),
+            nn.Dropout(0.3)
+        )
+        self.hidden2 = nn.Sequential(
+            nn.Linear(512, 256),
+            nn.LeakyReLU(0.2),
+            nn.Dropout(0.3)
+        )
+        self.out = nn.Sequential(
+            nn.Linear(256, n_out),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        x = self.hidden0(x)
+        x = self.hidden1(x)
+        x = self.hidden2(x)
+        x = self.out(x)
+        return x
+discriminator = DiscriminatorNet()
+
+
+# In[17]:
+
+
+def images_to_vectors(images):
+    return images.view(images.size(0), 784)
+
+def vectors_to_images(vectors):
+    return vectors.view(vectors.size(0), 1, 28, 28)
+
+
+# In[18]:
+
+
+class GeneratorNet(nn.Module):
+    """
+    A three hidden-layer generative neural network
+    """
+    def __init__(self):
+        super(GeneratorNet, self).__init__()
+        n_features = 100
+        n_out = 784
+        
+        self.hidden0 = nn.Sequential(
+            nn.Linear(n_features, 256),
+            nn.LeakyReLU(0.2)
+        )
+        self.hidden1 = nn.Sequential(            
+            nn.Linear(256, 512),
+            nn.LeakyReLU(0.2)
+        )
+        self.hidden2 = nn.Sequential(
+            nn.Linear(512, 1024),
+            nn.LeakyReLU(0.2)
+        )
+        
+        self.out = nn.Sequential(
+            nn.Linear(1024, n_out),
+            nn.Tanh()
+        )
+
+    def forward(self, x):
+        x = self.hidden0(x)
+        x = self.hidden1(x)
+        x = self.hidden2(x)
+        x = self.out(x)
+        return x
+generator = GeneratorNet()
+
+
+# In[19]:
+
+
+def noise(size):
+    '''
+    Generates a 1-d vector of gaussian sampled random values
+    '''
+    n = Variable(torch.randn(size, 100))
+    return n
+
+
+# In[20]:
+
+
+d_optimizer = optim.Adam(discriminator.parameters(), lr=0.0002)
+g_optimizer = optim.Adam(generator.parameters(), lr=0.0002)
+
+
+# In[21]:
+
+
+loss = nn.BCELoss()
+
+
+# In[22]:
+
+
+def ones_target(size):
+    '''
+    Tensor containing ones, with shape = size
+    '''
+    data = Variable(torch.ones(size, 1))
+    return data
+
+def zeros_target(size):
+    '''
+    Tensor containing zeros, with shape = size
+    '''
+    data = Variable(torch.zeros(size, 1))
+    return data
+
+
+# In[23]:
+
+
+def train_discriminator(optimizer, real_data, fake_data):
+    N = real_data.size(0)
+    # Reset gradients
+    optimizer.zero_grad()
+    
+    # 1.1 Train on Real Data
+    prediction_real = discriminator(real_data)
+    # Calculate error and backpropagate
+    error_real = loss(prediction_real, ones_target(N) )
+    error_real.backward()
+
+    # 1.2 Train on Fake Data
+    prediction_fake = discriminator(fake_data)
+    # Calculate error and backpropagate
+    error_fake = loss(prediction_fake, zeros_target(N))
+    error_fake.backward()
+    
+    # 1.3 Update weights with gradients
+    optimizer.step()
+    
+    # Return error and predictions for real and fake inputs
+    return error_real + error_fake, prediction_real, prediction_fake
+
+
+# In[24]:
+
+
+def train_generator(optimizer, fake_data):
+    N = fake_data.size(0)
+    # Reset gradients
+    optimizer.zero_grad()
+    # Sample noise and generate fake data
+    prediction = discriminator(fake_data)
+    # Calculate error and backpropagate
+    error = loss(prediction, ones_target(N))
+    error.backward()
+    # Update weights with gradients
+    optimizer.step()
+    # Return error
+    return error
+
+
+# In[25]:
+
+
+num_test_samples = 16
+test_noise = noise(num_test_samples)
+
+
+# In[27]:
+
+
+# Create logger instance
+logger = Logger(model_name='VGAN', data_name='MNIST')
+# Total number of epochs to train
+num_epochs = 200
+for epoch in range(num_epochs):
+    for n_batch, (real_batch,_) in enumerate(data_loader):
+        N = real_batch.size(0) #100
+        # 1. Train Discriminator
+        real_data = Variable(images_to_vectors(real_batch))   #tensor = 100X784
+        
+        # Generate fake data and detach 
+        # (so gradients are not calculated for generator)
+        fake_data = generator(noise(N)).detach()              #tensor = 100X784
+        
+        # Train D
+        d_error, d_pred_real, d_pred_fake = train_discriminator(d_optimizer, real_data, fake_data)
+
+        # 2. Train Generator
+        # Generate fake data
+        fake_data = generator(noise(N))
+        
+        # Train G
+        g_error = train_generator(g_optimizer, fake_data)
+        # Log batch error
+        logger.log(d_error, g_error, epoch, n_batch, num_batches)
+        # Display Progress every few batches
+        if (n_batch) % 100 == 0: 
+            test_images = vectors_to_images(generator(test_noise))
+            test_images = test_images.data
+            logger.log_images(
+                test_images, num_test_samples, 
+                epoch, n_batch, num_batches
+            );
+            # Display status Logs
+            logger.display_status(
+                epoch, num_epochs, n_batch, num_batches,
+                d_error, g_error, d_pred_real, d_pred_fake
+            )
+
